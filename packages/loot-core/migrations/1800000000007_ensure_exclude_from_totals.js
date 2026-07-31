80742de2729a77ae5ebdec3f814184cd68192a87
xpbach2508 <xpbach2508@gmail.com>
2026-08-01 01:51:54 +0700

    chore: standard sql migration for exclude_from_totals

--- packages/loot-core/migrations/1800000000007_ensure_exclude_from_totals.js
+++
@@ -1,9 +0,0 @@
-export default async function run(db) {
-  const columns = db.runQuery('PRAGMA table_info(accounts)', [], true);
-  const hasColumn = columns.some(c => c.name === 'exclude_from_totals');
-  if (!hasColumn) {
-    db.runQuery(
-      'ALTER TABLE accounts ADD COLUMN exclude_from_totals INTEGER DEFAULT 0',
-    );
-  }
-}
